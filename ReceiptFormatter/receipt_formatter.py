f58add5b592cb0193e11c4f49104c46716fef833
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2026-08-19 10:59:14 +0100

    Add student dictionary and related operations to receipt_formatter.py

--- ReceiptFormatter/receipt_formatter.py
+++ ReceiptFormatter/receipt_formatter.py
@@ -1,3 +1,6 @@
+import email
+
+
 def receipt_formatter(name, quantity, price):
     quantity = float(quantity)
     price = float(price)
@@ -6,3 +9,27 @@ def receipt_formatter(name, quantity, price):
     total = round(subtotal + tax, 2)
     return f"Customer: {name}\nSubtotal: {subtotal}\nTax: {tax}\nTotal: {total}"
     pass
+
+
+student = {
+    "Name": "Emmanuel",
+    "Age": 25,
+    "Email" : "emmanuel@example.com",
+    "courses": ["Python", "SQL", "Django"]    
+}
+
+print(student["Email"])
+
+student["courses"].append("Data Science")
+
+student["courses"].remove("SQL")
+
+"Python" in student["courses"]
+
+student["Age"] = 26
+student["courses"].append("JavaScript")
+print("Django" in student["courses"])
+
+for course in student["courses"]:
+    print(course)
+    
